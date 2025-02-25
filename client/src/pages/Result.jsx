2a096ab219f538340dbcb1239689f408e294856b
Khalia Howard <Khalia@Administrators-MacBook-Pro.local>
2025-02-25 09:18:46 -0500

    feat: result page

---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+
+const Result = () => {
+    return (
+        <div className="container center-align">
+            <h1 className="teal-text">lrnr</h1>
+            <p className="flow-text">Questions Right: 0111</p>
+            <button className="btn waves-effect waves-light">TRY ANOTHER QUIZ</button>
+        </div>
+    );
+};
+
+export default Result;
